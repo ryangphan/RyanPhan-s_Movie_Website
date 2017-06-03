c64c5c2040d3a38b8de15e4ee32aeec5852755ca
tphan40 <tphan40@gatech.edu>
2017-06-03 16:40:32 +0700

    Please read the README for more information about this project

---
+++ README.md
@@ -0,0 +1,35 @@
+# PROJECT 1: MOVIE TRAILER WEBSITE
+
+### by Ryan Phan
+
+Project Movie Trailer Website is a part of the [Udacity Full Stack Web Developer Nanodegree](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004).
+
+The project consist of the following files:
+  - media.py : the Movie Class that will construct an object to store all information from movie
+  - entertainment_center.py : a collection of information about all movies
+  - fresh_tomatoes.py : generate a movie webiste to be displayed in a webbrowswer
+
+### Installation
+
+  - Python 2.x is required to run this project. To download, please click [here](https://www.python.org/downloads/release/python-2713/)
+
+### How To Run This Project
+
+  - Download the project zip file to you computer and unzip the file. Or clone this repository to your desktop.
+  - Open your computer browser, navigate to the project directory and type in the following command
+```sh
+python entertainment_center.py
+```
+  - If the project compiles successfully, a file name Ryan Phan Movie Trailer.html will be generated. In addition, the defaul webbrowswer should launch a new tab display the movie trailer website
+
+### Miscellaneous
+This README document is based on a template suggested by PhilipCoach in this Udacity forum [post](https://discussions.udacity.com/t/readme-files-in-project-1/23524/2)
+
+
+
+
+
+
+
+
+ 
---
+++ entertaiment_center.py
@@ -0,0 +1,32 @@
+""" This file contains all information about each movies.
+    media.py will take these information and display
+    them on the website
+"""
+import media
+import fresh_tomatoes
+
+""" Initializing movie objects with information such as: title,
+    storyline, poster image url, and trailer youtube url
+"""
+
+# Create instances for movie: Pirate of the Carribean
+pirate = media.Movie("Pirate of the Carribean", "Story about Pirate Life",
+                     "https://upload.wikimedia.org/wikipedia/en/c/c6/On_Stranger_Tides_Poster.jpg",
+                     "https://www.youtube.com/watch?v=IPf4rGw3XHw")
+
+# Create instances for movie: Transformer
+transformer = media.Movie("Transformer", "Story about robots fighting each other",
+                          "https://upload.wikimedia.org/wikipedia/en/2/26/Transformers_The_Last_Knight_poster.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=uD5IJvKcbEQ")
+
+# Create instances for movie: Sherlock Holmes
+sherlock = media.Movie("Sherlock Holmes", "Film about Sherlock Holmes",
+                       "http://vignette3.wikia.nocookie.net/bakerstreet/images/d/db/Sherlock_holmes_ritchie.jpg/revision/latest?cb=20130202212947", # NOQA
+                       "https://www.youtube.com/watch?v=Egcx63-FfTE")
+
+
+# Add all of the movie in an array to display them on the website
+movies = [pirate, transformer, sherlock]
+
+# Open all of the movies in browser by using fresh_tomatoes.py
+fresh_tomatoes.open_movies_page(movies)
